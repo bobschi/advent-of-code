3817f00be676495f15b25a857e9aaebb65b98613
Florian Holzner <bobschi@lebobs.ch>
2023-12-07 18:10:21 +0100

    Added broken solution for day 1 part b.

--- 2023/day01.py
+++ 2023/day01.py
@@ -10,22 +10,49 @@ class Position(enum.IntEnum):
     LAST = -1
 
 
+WORD_TO_DIGIT = dict(
+    one="1",
+    two="2",
+    three="3",
+    four="4",
+    five="5",
+    six="6",
+    seven="7",
+    eight="8",
+    nine="9",
+)
+
+
+def part_specific_regex(part: aoc.Part) -> str:
+    regexes = {aoc.Part.A: r"\d", aoc.Part.B: rf"{'|'.join(WORD_TO_DIGIT.keys())}|\d"}
+    return regexes.get(part)
+
+
 def digit(input: str, position: Position, part: aoc.Part) -> str:
-    return re.findall("\d", input)[position.value]
+    result: str = re.findall(part_specific_regex(part), input)[position.value]
+
+    if not result.isdigit():
+        return WORD_TO_DIGIT.get(result)
+
+    return result
 
 
 def calibration_value(input: str, part: aoc.Part) -> int:
-    return int(digit(input, Position.FIRST, part) + digit(input, Position.LAST, part))
+    return int(digit(input, Position.LAST, part) + digit(input, Position.FIRST, part))
 
 
 def solve(input: list[str], part: aoc.Part) -> int:
     return sum(map(calibration_value, input, part * len(input)))
 
 
-def main(part: str) -> None:
+def main(part: str, submit: bool = False) -> None:
     part = aoc.Part(part)
     inputs = aocd.get_data(day=1, year=2023).split("\n")
-    aocd.submit(solve(inputs, part), part.value, day=1, year=2023)
+    result = solve(inputs, part)
+    if submit:
+        aocd.submit(result, part.value, day=1, year=2023)
+    else:
+        typer.echo(f"Result: {result}")
 
 
 if __name__ == "__main__":
--- 2023/test_day01.py
+++ 2023/test_day01.py
@@ -8,6 +8,19 @@ def part_a_examples() -> list[str]:
     return ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
 
 
+@pytest.fixture
+def part_b_examples() -> list[str]:
+    return [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen",
+    ]
+
+
 def test_first_digit_part_a(part_a_examples: list[str]) -> None:
     expected_first_digits = ["1", "3", "1", "7"]
 
@@ -31,3 +44,28 @@ def test_calibration_value_part_a(part_a_examples: list[str]) -> None:
 
 def test_solve_part_a(part_a_examples: list[str]) -> None:
     assert day01.solve(part_a_examples, aoc.Part.A) == 142
+
+
+def test_first_digit_part_b(part_b_examples: list[str]) -> None:
+    expected_first_digits = ["2", "8", "1", "2", "4", "1", "7"]
+
+    for input, expected in zip(part_b_examples, expected_first_digits):
+        assert day01.digit(input, day01.Position.FIRST, aoc.Part.B) == expected
+
+
+def test_last_digit_part_b(part_b_examples: list[str]) -> None:
+    expected_last_digits = ["9", "3", "3", "4", "2", "4", "6"]
+
+    for input, expected in zip(part_b_examples, expected_last_digits):
+        assert day01.digit(input, day01.Position.LAST, aoc.Part.B) == expected
+
+
+def test_calibration_value_part_b(part_b_examples: list[str]) -> None:
+    expected_calibration_values = [29, 83, 13, 24, 42, 14, 76]
+
+    for input, expected in zip(part_b_examples, expected_calibration_values):
+        assert day01.calibration_value(input, aoc.Part.B) == expected
+
+
+def test_solve_part_b(part_b_examples: list[str]) -> None:
+    assert day01.solve(part_b_examples, aoc.Part.B) == 281
