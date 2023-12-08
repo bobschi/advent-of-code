2b75acfd7dcf3f6f2a569b6d42bf2714b5a0ab41
Florian Holzner <bobschi@lebobs.ch>
2023-12-08 13:57:21 +0100

    Day 3: Fixed bugs in part a and solved part b.

--- 2023/aoc/day03.py
+++ 2023/aoc/day03.py
@@ -1,3 +1,4 @@
+import functools
 import re
 from dataclasses import dataclass
 
@@ -15,6 +16,7 @@ class Point:
 @dataclass
 class Number:
     start: Point
+    end: Point
     value: str
 
 
@@ -24,9 +26,10 @@ def parse_numbers(inputs: list[str]) -> list[Number]:
         for number in re.finditer(r"\d+", line):
             start = Point(number.start(), y)
             value = number.group()
+            end = Point(start.x + len(value) - 1, y)
 
-            if symbol_adjacent(inputs, start, Point(start.x + len(value) - 1, start.y)):
-                positions.append(Number(start, value))
+            if symbol_adjacent(inputs, start, end):
+                positions.append(Number(start, end, value))
 
     return positions
 
@@ -41,9 +44,37 @@ def symbol_adjacent(inputs: list[str], start: Point, end: Point) -> bool:
     return False
 
 
-def solve(inputs: list[str]) -> int:
+def solve_part_a(inputs: list[str]) -> int:
     return sum(map(lambda x: int(x.value), parse_numbers(inputs)))
 
 
+def in_eight_neighborhood(point: Point, number: Number) -> bool:
+    for x in range(number.start.x - 1, number.end.x + 2):
+        if point.x == x and point.y in range(number.start.y - 1, number.start.y + 2):
+            return True
+
+    return False
+
+
+def find_gear_ratios(inputs: list[str]) -> list[int]:
+    numbers = parse_numbers(inputs)
+    gear_ratios = list()
+    for y, line in enumerate(inputs):
+        for potential_gear in re.finditer(r"\*{1}", line):
+            gear_pos = Point(potential_gear.start(), y)
+            pot_nums = list(filter(lambda n: n.start.y in range(y - 1, y + 2), numbers))
+            is_neighbor = functools.partial(in_eight_neighborhood, gear_pos)
+            pot_nums = list(filter(is_neighbor, pot_nums))
+
+            if len(pot_nums) == 2:
+                gear_ratios.append(int(pot_nums[0].value) * int(pot_nums[1].value))
+
+    return gear_ratios
+
+
+def solve_part_b(inputs: list[str]) -> int:
+    return sum(find_gear_ratios(inputs))
+
+
 if __name__ == "__main__":
-    typer.run(aoc.get_cli(solve, None, 3))
+    typer.run(aoc.get_cli(solve_part_a, solve_part_b, 3))
--- 2023/aoc/tests/test_day03.py
+++ 2023/aoc/tests/test_day03.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc.day03 import Number, Point, parse_numbers, solve
+from aoc.day03 import Number, Point, find_gear_ratios, parse_numbers, solve_part_a
 
 
 @pytest.fixture
@@ -21,18 +21,24 @@ def example_schematic() -> list[str]:
 
 def test_parse_numbers(example_schematic) -> None:
     example_positions = [
-        Number(Point(0, 0), "467"),
-        Number(Point(2, 2), "35"),
-        Number(Point(6, 2), "633"),
-        Number(Point(0, 4), "617"),
-        Number(Point(2, 6), "592"),
-        Number(Point(6, 7), "755"),
-        Number(Point(1, 9), "664"),
-        Number(Point(5, 9), "598"),
+        Number(Point(0, 0), Point(2, 0), "467"),
+        Number(Point(2, 2), Point(3, 2), "35"),
+        Number(Point(6, 2), Point(8, 2), "633"),
+        Number(Point(0, 4), Point(2, 4), "617"),
+        Number(Point(2, 6), Point(4, 6), "592"),
+        Number(Point(6, 7), Point(8, 7), "755"),
+        Number(Point(1, 9), Point(3, 9), "664"),
+        Number(Point(5, 9), Point(7, 9), "598"),
     ]
 
     assert parse_numbers(example_schematic) == example_positions
 
 
 def test_solve(example_schematic) -> None:
-    assert solve(example_schematic) == 4361
+    assert solve_part_a(example_schematic) == 4361
+
+
+def test_find_gear_ratios(example_schematic) -> None:
+    expected_ratios = [16345, 451490]
+
+    assert find_gear_ratios(example_schematic) == expected_ratios
