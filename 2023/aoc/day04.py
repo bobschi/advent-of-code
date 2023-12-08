95243384062f288fb5b5a79c10afc5e08eb13da9
Florian Holzner <bobschi@lebobs.ch>
2023-12-08 14:36:48 +0100

    Refactored day 4 part a to using dataclass.

--- 2023/aoc/day04.py
+++ 2023/aoc/day04.py
@@ -1,18 +1,34 @@
+from dataclasses import dataclass
+
 import typer
 
 from aoc.infra import get_cli
 
 
-def winning_numbers(card: str) -> list[int]:
-    winning_numbers, guessed_numbers = card.split("|")
-    winning_numbers = set(map(int, winning_numbers.split()))
-    guessed_numbers = set(map(int, guessed_numbers.split()))
+@dataclass
+class Card:
+    id: int
+    winners: list[int]
+    guesses: list[int]
 
-    return list(sorted(guessed_numbers & winning_numbers))
+
+def parse_card(line: str) -> Card:
+    card_id, winners_and_guesses = line.split(": ")
+    _, id = card_id.split()
+    winners, guesses = winners_and_guesses.split("|")
+
+    id = int(id)
+    winners = list(map(int, winners.split()))
+    guesses = list(map(int, guesses.split()))
+
+    return Card(id, winners, guesses)
 
 
-def score(card: str) -> int:
-    _, card = card.split(": ")
+def winning_numbers(card: Card) -> list[int]:
+    return list(sorted(set(card.guesses) & set(card.winners)))
+
+
+def score(card: Card) -> int:
     cards_winning_numbers = winning_numbers(card)
     if len(cards_winning_numbers) > 0:
         return 2 ** (len(cards_winning_numbers) - 1)
@@ -21,7 +37,7 @@ def score(card: str) -> int:
 
 
 def solve_part_a(cards: list[str]) -> int:
-    return sum(map(score, cards))
+    return sum(map(score, map(parse_card, cards)))
 
 
 if __name__ == "__main__":
--- 2023/aoc/tests/test_day04.py
+++ 2023/aoc/tests/test_day04.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc.day04 import score, solve_part_a, winning_numbers
+from aoc.day04 import Card, parse_card, score, solve_part_a, winning_numbers
 
 
 @pytest.fixture
@@ -15,22 +15,39 @@ def example_scratch_cards() -> list[str]:
     ]
 
 
-def test_winning_numbers(example_scratch_cards) -> None:
-    assert winning_numbers(example_scratch_cards[0][7:]) == [17, 48, 83, 86]
-    assert winning_numbers(example_scratch_cards[1][7:]) == [32, 61]
-    assert winning_numbers(example_scratch_cards[2][7:]) == [1, 21]
-    assert winning_numbers(example_scratch_cards[3][7:]) == [84]
-    assert winning_numbers(example_scratch_cards[4][7:]) == []
-    assert winning_numbers(example_scratch_cards[5][7:]) == []
+@pytest.fixture
+def example_expected_cards() -> list[Card]:
+    return [
+        Card(1, [41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]),
+        Card(2, [13, 32, 20, 16, 61], [61, 30, 68, 82, 17, 32, 24, 19]),
+        Card(3, [1, 21, 53, 59, 44], [69, 82, 63, 72, 16, 21, 14, 1]),
+        Card(4, [41, 92, 73, 84, 69], [59, 84, 76, 51, 58, 5, 54, 83]),
+        Card(5, [87, 83, 26, 28, 32], [88, 30, 70, 12, 93, 22, 82, 36]),
+        Card(6, [31, 18, 13, 56, 72], [74, 77, 10, 23, 35, 67, 36, 11]),
+    ]
 
 
-def test_score(example_scratch_cards) -> None:
-    assert score(example_scratch_cards[0]) == 8
-    assert score(example_scratch_cards[1]) == 2
-    assert score(example_scratch_cards[2]) == 2
-    assert score(example_scratch_cards[3]) == 1
-    assert score(example_scratch_cards[4]) == 0
-    assert score(example_scratch_cards[5]) == 0
+def test_parse_card(example_scratch_cards, example_expected_cards) -> None:
+    for i in range(6):
+        assert parse_card(example_scratch_cards[i]) == example_expected_cards[i]
+
+
+def test_winning_numbers(example_expected_cards) -> None:
+    assert winning_numbers(example_expected_cards[0]) == [17, 48, 83, 86]
+    assert winning_numbers(example_expected_cards[1]) == [32, 61]
+    assert winning_numbers(example_expected_cards[2]) == [1, 21]
+    assert winning_numbers(example_expected_cards[3]) == [84]
+    assert winning_numbers(example_expected_cards[4]) == []
+    assert winning_numbers(example_expected_cards[5]) == []
+
+
+def test_score(example_expected_cards) -> None:
+    assert score(example_expected_cards[0]) == 8
+    assert score(example_expected_cards[1]) == 2
+    assert score(example_expected_cards[2]) == 2
+    assert score(example_expected_cards[3]) == 1
+    assert score(example_expected_cards[4]) == 0
+    assert score(example_expected_cards[5]) == 0
 
 
 def test_solve_part_a(example_scratch_cards) -> None:
