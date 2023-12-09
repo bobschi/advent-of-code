b17f8023fde3114c00da10e8d6d56537b5628c40
Florian Holzner <bobschi@lebobs.ch>
2023-12-09 16:02:54 +0100

    Added a progress animation to CLI.

--- 2023/aoc/infra.py
+++ 2023/aoc/infra.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 import aocd
 import typer
+from rich.progress import Progress, SpinnerColumn, TextColumn, TimeElapsedColumn
 
 YEAR = 2023
 
@@ -14,18 +15,27 @@ class Part(enum.StrEnum):
 
 Solver = Callable[[list[str]], int]
 
+progress = Progress(
+    SpinnerColumn(),
+    TextColumn("[progress.description]{task.description}"),
+    TimeElapsedColumn(),
+    transient=True,
+)
+
 
 def get_cli(solver_a: Solver, solver_b: Solver | None, day: int):
     def cli(part: str, submit: bool = False):
         p = Part(part)
         inputs = aocd.get_data(day=day, year=YEAR).split("\n")
 
-        if p == Part.A:
-            result = solver_a(inputs)
-        elif solver_b is not None:
-            result = solver_b(inputs)
-        else:
-            result = "No solver for part B"
+        with progress:
+            progress.add_task(description="Solving ...", total=None)
+            if p == Part.A:
+                result = solver_a(inputs)
+            elif solver_b is not None:
+                result = solver_b(inputs)
+            else:
+                result = "No solver for part B"
 
         if submit:
             aocd.submit(result, p.value, day=day, year=YEAR)
