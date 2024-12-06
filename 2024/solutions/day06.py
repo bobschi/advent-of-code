53d18c2f36c4eadd4fdbcf1e2f9cb55e62b85ea0
Florian 'bobschi' Holzner <bobschi@lebobs.ch>
2024-12-06 08:47:07 +0100

    solve day 6

--- 2024/solutions/day06.py
+++ 2024/solutions/day06.py
@@ -1,8 +1,172 @@
-GUARD = "^"
-OBSTRUCTION = "#"
+from collections import defaultdict, deque
+from copy import deepcopy
+from enum import StrEnum
+
+import aocd
+import typer
+from rich.progress import track
+
+from solutions.shared import Dir, Point
+
+
+class Marks(StrEnum):
+    GUARD = "^"
+    OBSTRUCTION = "#"
+    VISITED = "X"
+    NEW_OBSTRUCTION = "O"
+
+
+app = typer.Typer()
+
+type Movements = list[Point]
+
 
 class Map:
-    fields:list[list[str]]
+    fields: list[list[str]]
 
-def process_data(data : str)->Map:
-    ...
+    def __init__(self, fields: list[list[str]]) -> None:
+        self.fields = fields
+
+    def __eq__(self, other) -> bool:
+        return self.fields == other.fields
+
+    def __repr__(self) -> str:
+        return f"Map {len(self.fields)}x{len(self.fields[0])}"
+
+    def get(self, coords: Point) -> str:
+        return self.fields[coords.x][coords.y]
+
+    def len_x(self) -> int:
+        return len(self.fields)
+
+    def len_y(self) -> int:
+        return len(self.fields[0])
+
+    def set(self, coords: Point, mark: str) -> None:
+        self.fields[coords.x][coords.y] = mark
+
+    def visit(self, coords: Point) -> None:
+        self.set(coords, Marks.VISITED)
+
+    def obstruct(self, coords: Point) -> None:
+        self.set(coords, Marks.NEW_OBSTRUCTION)
+
+    def is_obstructed(self, coords: Point) -> bool:
+        return self.get(coords) in [Marks.OBSTRUCTION, Marks.NEW_OBSTRUCTION]
+
+    def new_obstruction_position(self) -> Point:
+        for x, line in enumerate(self.fields):
+            if Marks.NEW_OBSTRUCTION in line:
+                return Point(x, line.index(Marks.NEW_OBSTRUCTION))
+        return Point(-1, -1)
+
+
+class Route:
+    the_map: Map
+    movements: Movements
+    directions: dict[Point, list[Dir]]
+    is_loop: bool = False
+
+    def __init__(self, the_map: Map) -> None:
+        self.the_map = the_map
+        self.movements = []
+        self.directions = defaultdict(list)
+
+    def add(self, position: Point, direction: Dir) -> None:
+        self.movements.append(position)
+        self.directions[position].append(direction)
+
+
+def is_loop(position: Point, direction: Dir, route: Route) -> bool:
+    return direction in route.directions[position]
+
+
+def in_bounds(the_map: Map, position: Point) -> bool:
+    return 0 <= position.x < the_map.len_x() and 0 <= position.y < the_map.len_y()
+
+
+def process_data(data: str) -> Map:
+    return Map([list(line) for line in data.split("\n")])
+
+
+def guard_position(the_map: Map) -> Point:
+    for x, line in enumerate(the_map.fields):
+        for y, cell in enumerate(line):
+            if cell == Marks.GUARD:
+                return Point(x, y)
+
+    return Point(-1, -1)
+
+
+def calculate_guard_route(the_map: Map) -> Route:
+    route = Route(the_map)
+    possible_directions = deque(Dir.cardinals())
+    current_direction = possible_directions.popleft()
+    current_position = guard_position(the_map)
+
+    while in_bounds(the_map, current_position):
+        if is_loop(current_position, current_direction, route):
+            route.is_loop = True
+            break
+        if the_map.is_obstructed(current_position):
+            current_position = current_position - current_direction.value
+            possible_directions.append(current_direction)
+            current_direction = possible_directions.popleft()
+        else:
+            route.add(current_position, current_direction)
+        current_position = current_position + current_direction.value
+
+    return route
+
+
+def find_loops(the_map: Map) -> list[Route]:
+    initial_route = calculate_guard_route(the_map)
+    loops = []
+
+    for position in track(initial_route.movements):
+        if the_map.get(position) in [Marks.GUARD, Marks.OBSTRUCTION]:
+            continue
+        new_map = Map(deepcopy(the_map.fields))
+        new_map.obstruct(position)
+        potential_route = calculate_guard_route(new_map)
+        if potential_route.is_loop:
+            loops.append(potential_route)
+
+    return loops
+
+
+def visualize_route(the_map: Map, route: Route | None = None) -> None:
+    visualize_this = Map(deepcopy(the_map.fields))
+    if route:
+        if route.is_loop:
+            print("Is loop.")
+        else:
+            print("No loop.")
+        for position in route.movements:
+            visualize_this.visit(position)
+    for line in visualize_this.fields:
+        print("".join(line))
+
+
+def possible_new_obstructions(loops: list[Route]) -> set[Point]:
+    return {loop.the_map.new_obstruction_position() for loop in loops}
+
+
+@app.command()
+def part1() -> None:
+    data = aocd.get_data(day=6, year=2024)
+    the_map = process_data(data)
+    route = calculate_guard_route(the_map)
+    aocd.submit(answer=str(len(set(route.movements))), part="a", day=6, year=2024)
+
+
+@app.command()
+def part2() -> None:
+    data = aocd.get_data(day=6, year=2024)
+    the_map = process_data(data)
+    new_obstructions = possible_new_obstructions(find_loops(the_map))
+    aocd.submit(answer=str(len(new_obstructions)), part="b", day=6, year=2024)
+
+
+if __name__ == "__main__":
+    app()
--- 2024/tests/test_day06.py
+++ 2024/tests/test_day06.py
@@ -1,17 +1,102 @@
 import pytest
 
+from solutions.day06 import (
+    Map,
+    find_loops,
+    possible_new_obstructions,
+    process_data,
+    guard_position,
+    Point,
+    in_bounds,
+    calculate_guard_route,
+)
+
 
 @pytest.fixture
 def example_input() -> str:
     return (
-        "....#....."
-        ".........#"
-        ".........."
-        "..#......."
-        ".......#.."
-        ".........."
-        ".#..^....."
-        "........#."
-        "#........."
+        "....#.....\n"
+        ".........#\n"
+        "..........\n"
+        "..#.......\n"
+        ".......#..\n"
+        "..........\n"
+        ".#..^.....\n"
+        "........#.\n"
+        "#.........\n"
         "......#..."
     )
+
+
+@pytest.fixture
+def processed_map() -> Map:
+    return Map(
+        [
+            [".", ".", ".", ".", "#", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", "#", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "#", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", "#", ".", ".", "^", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", ".", "#", "."],
+            ["#", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", "#", ".", ".", "."],
+        ]
+    )
+
+
+def test_import_data(example_input: str, processed_map: Map) -> None:
+    assert process_data(example_input) == processed_map
+
+
+def test_guard_position(processed_map: Map) -> None:
+    assert guard_position(processed_map) == Point(6, 4)
+
+
+def test_in_bounds(processed_map: Map) -> None:
+    bottom_right = Point(processed_map.len_x(), processed_map.len_y()) - Point(1, 1)
+    assert in_bounds(processed_map, Point(0, 0))
+    assert in_bounds(processed_map, bottom_right)
+    assert in_bounds(processed_map, Point(5, 5))
+
+    assert not in_bounds(processed_map, Point(-1, -1))
+    assert not in_bounds(processed_map, Point(0, -1))
+    assert not in_bounds(processed_map, Point(-1, 0))
+    assert not in_bounds(processed_map, Point(9999, 9999))
+    assert not in_bounds(processed_map, Point(0, 9999))
+    assert not in_bounds(processed_map, Point(9999, 0))
+
+
+def test_calculate_guard_route(processed_map: Map) -> None:
+    route = calculate_guard_route(processed_map)
+    assert len(set(route.movements)) == 41
+
+
+def test_find_loops(processed_map: Map) -> None:
+    expected_positions = {
+        Point(6, 3),
+        Point(7, 6),
+        Point(7, 7),
+        Point(8, 1),
+        Point(8, 3),
+        Point(9, 7),
+    }
+    loops = find_loops(processed_map)
+    positions = {loop.the_map.new_obstruction_position() for loop in loops}
+
+    assert positions == expected_positions
+
+
+def test_possible_new_obstructions(processed_map: Map) -> None:
+    expected_positions = {
+        Point(6, 3),
+        Point(7, 6),
+        Point(7, 7),
+        Point(8, 1),
+        Point(8, 3),
+        Point(9, 7),
+    }
+    new_obstructions = possible_new_obstructions(find_loops(processed_map))
+
+    assert new_obstructions == expected_positions
